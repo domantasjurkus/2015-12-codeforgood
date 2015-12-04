adbeb38c34fcefe50eb1c0d50ad164d46fe2a4c6
domantasjurkus <domantas.jurkus@gmail.com>
2015-12-04 22:55:15 +0000

    Add item controller

--- route21/api/controllers/ItemController.js
+++ route21/api/controllers/ItemController.js
@@ -4,9 +4,43 @@
  * @description :: Server-side logic for managing Items
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
+var http = require('http');
 
 module.exports = {
+
+  insert: function(req, res) {
+    var url = req.param('url');
+    res.send(url);
+
+    var options = {
+      host : url,
+      port : 80,
+      path : "/",
+      method : 'GET'
+    };
+
+    var data = "";
+
+    var req = http.request(options, function(res){
+      res.on('data', function(chunk){
+        console.log("We got some data");
+      });
+
+      res.on('end', function(){
+        console.log("Request ended");
+        res.send(data);
+      });
+
+      res.on('error', function(e){
+        console.log(e.message);
+      });
+
+    });
+
+  },
+
 	find: function(req, res) {
 		res.send('Hello World');
 	}
+
 };
