2e4c3e977636c8a08cd392887f21b3a3d2d14ec5
Martin Kubat <martinkubat@icloud.com>
2015-12-04 21:55:08 +0000

    Made User api

---
+++ route21/api/models/User.js
@@ -0,0 +1,34 @@
+/**
+* User.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+    username: {
+      type: 'string',
+      unique: true
+    },
+
+    facebookId: {
+      type: 'string',
+    },
+
+    firstname: {
+      type: 'string',
+    },
+
+    lastname: {
+      type: 'string',
+    },
+
+    email: {
+      type: 'string',
+    },
+
+
+  }
+};
