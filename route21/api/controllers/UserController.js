aff81cec07d8cd0e073c7603202f287eaf38a4fc
Martin Kubat <martinkubat@icloud.com>
2015-12-05 04:34:59 +0000

    More fixes

--- route21/api/controllers/UserController.js
+++ route21/api/controllers/UserController.js
@@ -10,21 +10,28 @@ var passport = require('passport');
 module.exports = {
 
   facebook: function (req, res, next) {
-     passport.authenticate('facebook', { scope: ['email', 'public_profile']},
-       function(req, res){
-         console.log('user/facebook', res);
-         // The request will be redirected to Facebook for authentication, so this
-         // function will not be called.
-       });
-	  },
 
-  callback: function (req, res) {
-    passport.authenticate('facebook', { failureRedirect: '/login' },
-    function(req, res) {
-      console.log('user/callback');
-      // Successful authentication, redirect home.
-      res.redirect('/');
-    });
+    passport.authenticate('facebook', { scope: ['email', 'public_profile']},
+       function (err, user) {
+           req.logIn(user, function (err) {
+           if(err) {
+               req.session.flash = 'There was an error';
+               req.redirect('user/login');
+           } else {
+               req.session.user = user;
+               req.redirect('/user/dashboard');
+           }
+       });
+   })(req, res, next);
+  },
+
+  callback: function (req, res, next) {
+    console.log('user/callback');
+    passport.authenticate('facebook', {successRedirect: '/', failureRedirect: 'user/fail'})(req, res, next);
 	},
 
+
+  fail: function(req, res) {
+    res.send('Fail');
+  }
 };
