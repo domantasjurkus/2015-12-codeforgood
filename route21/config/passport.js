f51e12e86ac8518182b229b853f9e4a48866114b
Martin Kubat <martinkubat@icloud.com>
2015-12-05 03:58:13 +0000

    Tried to fix auth

--- route21/config/passport.js
+++ route21/config/passport.js
@@ -4,12 +4,22 @@ var FacebookStrategy = require('passport-facebook').Strategy;
 passport.use(new FacebookStrategy({
   clientID: 1694746604094531,
   clientSecret: "47f295c2e4cf88e5c352ef315017a0a0",
-  callbackURL: "http://localhost:1337/auth/callback",
-  enableProof: false
+  callbackURL: "http://route21.com:1337/user/callback",
+  enableProof: false,
 },
 function(accessToken, refreshToken, profile, done) {
+  console.log('The profile is ', profile, done);
   User.findOrCreate({ facebookId: profile.id }, function (err, user) {
     return done(err, user);
   });
 }
 ));
+
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
